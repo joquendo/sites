ee0f13126cd4b8437a4c984f8a983fc1fcd33b90
James Oquendo <james.oquendo@gmail.com>
2020-04-30 23:10:19 -0700

    clear directory

--- jamesoquendo/projects/lamps-plus/main.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-	var $ul = $('#grocery-list');
-	$('button').click( function sortOrder() {
-		$('li', $ul).sort( function() {
-			return (Math.round( Math.random() ) - 0.5 )
-		}).appendTo($ul);
-	})
-});
