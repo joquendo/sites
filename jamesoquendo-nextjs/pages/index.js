e082a11e717389c59d4cd74e00bccdebd736eabd
James Oquendo <james.oquendo@gmail.com>
2020-05-17 17:06:42 -0700

    Add Jest testing

--- jamesoquendo-nextjs/pages/index.js
+++ jamesoquendo-nextjs/pages/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 const HomePage = () => {
   return (
-    <div>Welcome to my homepage using Next.js!</div>
+    <div>
+      <h1>James Oquendo</h1>
+      <span>Software Engineer</span>
+      <p>Web Developer, programmer, bread baker, health enthusiast, and vegan advocate.</p>
+    </div>
+    
   )
 }
 
