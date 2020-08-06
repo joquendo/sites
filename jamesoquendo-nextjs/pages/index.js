f85f36c60243c2b58a254a9dc4ba0a5c1a3c26d8
James Oquendo <james.oquendo@gmail.com>
2020-08-06 09:31:37 -0700

    App component

--- jamesoquendo-nextjs/pages/index.js
+++ jamesoquendo-nextjs/pages/index.js
@@ -1,16 +1,8 @@
 import React from 'react';
+import App from '../src/App';
 
-const HomePage = () => {
-  return (
-    <div>
-      <h1>James Oquendo</h1>
-      <span>Software Engineer</span>
-      <p>Web Developer, programmer, bread baker, health enthusiast, and vegan advocate.</p>
-      <a href="/about">About</a>
-      <a href="/projects">Projects</a>
-    </div>
-    
-  )
+const index = () => {
+  return <App pageName="Home" />;
 }
 
-export default HomePage
+export default index;
